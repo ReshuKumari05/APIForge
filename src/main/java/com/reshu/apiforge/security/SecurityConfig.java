03065f853523a4f31880280471907fa825a66e86
Reshu Kumari <reshukumari0504@gmail.com>
2026-07-28 23:02:52 +0530

    Add public home redirect to Swagger

--- src/main/java/com/reshu/apiforge/security/SecurityConfig.java
+++ src/main/java/com/reshu/apiforge/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/",
                                 "/api/users/register",
                                 "/api/users/login",
                                 "/v3/api-docs/**",
@@ -51,6 +52,7 @@ public class SecurityConfig {
 
         return http.build();
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
